c6415c69d6e971431c1c6391c017e87576aa208d
MERNaka <menakajayaraj@gmail.com>
2024-02-17 18:55:27 +0530

    connected to DB

--- api/index.js
+++ api/index.js
@@ -1,6 +1,17 @@
 import express from 'express'
+import mongoose from 'mongoose'
+import dotenv from 'dotenv'
+dotenv.config()
 
-const app = express();
+mongoose
+    .connect(`mongodb+srv://menaka:${process.env.MONGODB_PASSWORD}@cluster0.fwdfgwh.mongodb.net/?retryWrites=true&w=majority`)
+    .then(()=>{
+        console.log("connected to MongoDB");
+    })
+    .catch((err)=>{
+        console.log(err);
+    })
+    const app = express();
 
 app.listen(3000, ()=>{
     console.log('listening to 3000!!!')
