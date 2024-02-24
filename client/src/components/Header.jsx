d6be124ea997c7f6a18d3b0cbdc12c59963fa94e
MERNaka <menakajayaraj@gmail.com>
2024-02-24 22:01:28 +0530

    updated header component

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,8 +1,10 @@
 import React from 'react';
 import {FaSearch} from 'react-icons/fa'
+import { useSelector } from 'react-redux';
 import {Link} from 'react-router-dom'
 
 const Header = () => {
+  const {currentUser} = useSelector((state)=>state.user)
   return (
     <header className='bg-slate-200 shadow-md'>
       <div className='flex justify-between items-center max-w-6xl mx-auto p-3'>
@@ -24,7 +26,11 @@ const Header = () => {
             <li className='hidden sm:inline text-slate-700 hover:underline'>About</li>
           </Link>
           <Link to='/sign-in'>
-            <li className='hidden sm:inline text-slate-700 hover:underline'>Sign in</li>
+            {currentUser 
+              ? ( <img className='rounded-full h-7 w-7 object-cover' src={currentUser.avatar} alt="profile" /> )
+              : ( <li className='hidden sm:inline text-slate-700 hover:underline'>Sign in</li> )
+              }
+            
           </Link>
         </ul>
       </div>
